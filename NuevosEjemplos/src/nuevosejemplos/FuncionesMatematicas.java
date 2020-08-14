e05252e251c63d1df7b9f44664814b7541b5c2e1
IrisRecinos <69639829+IrisRecinos@users.noreply.github.com>
2020-08-14 14:48:26 -0600

    Ejemplo de suma,resta y divicion
    Se añadio el primer ejemplo

---
+++ NuevosEjemplos/src/nuevosejemplos/FuncionesMatematicas.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevosejemplos;
+
+/**
+ *
+ * @author iris recinos
+ */
+public class FuncionesMatematicas {
+//      int dato1= 12;
+//  int dato2 = 10;
+//  int Resultado;
+//  
+//  Resultado = dato1+ dato2;
+//  System.out.println("El resultado es " + Resultado);
+  
+  int Suma(){
+          
+  int dato1= 12;
+  int dato2 = 10;
+  int Resultado;
+  
+  Resultado = dato1+ dato2;
+  
+  return Resultado;
+  
+  }
+  int Resta(){
+  int dato1=12;
+  int dato2= 10;
+  int Resultado;
+  
+  Resultado= dato1 - dato2;
+  
+  return Resultado;
+//  en esto es objeto hay dos funciones suma y resta
+//esta es la clase hijo
+//al cambiar cosas aca se cambian cosas en la clase
+   
+
+  }double divicion(){
+   int dato1=12;
+  int dato2= 10;
+  int Resultado;
+  
+  Resultado= dato1 / dato2;   
+  return Resultado;
+//  aca pusimos duble en divicion por que el resultado en diviciones aveces es decimal
+          
+  }
+  
+ 
+}
+        
+
+  
---
+++ NuevosEjemplos/src/nuevosejemplos/NuevosEjemplos.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevosejemplos;
+
+/**
+ *
+ * @author iris recinos
+ */
+public class NuevosEjemplos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+        FuncionesMatematicas Funciones =new FuncionesMatematicas();
+        
+        System.out.println(Funciones.divicion());
+//        donde va suma puede ir tambien resta divicion o multiplicacion 
+//funcionesMatematicas = la clase 
+//funciones =el objeto
+//las propiedades=dicicion, println o asi
+//TODO ESTO ES LA INSTANCIA OK.
+        
+      
+     
+       
+  
+        
+    }
+    
+}
